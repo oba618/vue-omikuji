9e36fa8f5955b10280eefaa2a8ee29986edcb4ba
oba618 <tomohiro900618@gmail.com>
2025-02-01 16:34:48 +0900

    Replace fortune templates
    おみくじ用にテンプレートを置換

---
+++ src/components/FortuneFooter.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <footer>
+    <div>
+      since 2025
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  padding: 2rem 0 0 0;
+}
+</style>
---
+++ src/components/FortuneHeader.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+    <div>
+      <h1>vue-omikuji</h1>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 2.5rem;
+}
+</style>
